fbae4c1ee6e8f79e6136897b05861c20868c8944
Niels Laukens <niels.laukens@vrt.be>
2013-05-09 19:42:04 +0200

    Moved log level numbers to match array index

--- include/loglevel.h
+++ include/loglevel.h
@@ -1,20 +1,30 @@
 #ifndef __LIBLOG_LOGLEVEL_H__
 #define __LIBLOG_LOGLEVEL_H__
 
-#define LOG_LEVEL_TRACE 1
-#define LOG_LEVEL_DEBUG 2
-#define LOG_LEVEL_INFO  3
-#define LOG_LEVEL_WARN  4
-#define LOG_LEVEL_ERROR 5
-#define LOG_LEVEL_FATAL 6
+#define LOG_LEVEL_TRACE 0
+#define LOG_LEVEL_DEBUG 1
+#define LOG_LEVEL_INFO  2
+#define LOG_LEVEL_WARN  3
+#define LOG_LEVEL_ERROR 4
+#define LOG_LEVEL_FATAL 5
 
 typedef enum LogLevel {
-	Trace=1,
+	Trace=0,
 	Debug,
 	Info,
 	Warn,
 	Error,
 	Fatal
 	} LogLevel;
+
+const char *LogLevelName[] = {
+	"trace",
+	"debug",
+	"info",
+	"warn",
+	"error",
+	"fatal"
+	};
+#define LOG_LEVEL_NAME_MAX_LENGTH 5
 
 #endif // defined __LIBLOG_LOGLEVEL_H__
