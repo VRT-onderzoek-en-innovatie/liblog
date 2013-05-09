e163597e9c4a8dd3e5b7ecd884346fdf384691b7
Niels Laukens <niels.laukens@vrt.be>
2013-05-09 19:42:24 +0200

    Added more location info to calls

--- include/log.h
+++ include/log.h
@@ -9,40 +9,42 @@
 #define LOG_LEVEL LOG_LEVEL_TRACE
 #endif
 
-int LogAtLevel(enum LogLevel level, const char *fmt, ...);
+int LogAtLevel(enum LogLevel level,
+               const char *file, const char *func, const int lineNum,
+               const char *fmt, ...);
 
 #if LOG_LEVEL <= LOG_LEVEL_TRACE
-#define LogTrace(fmt, ...) LogAtLevel(Trace, fmt, ## __VA_ARGS__)
+#define LogTrace(fmt, ...) LogAtLevel(Trace, __FILE__, __func__, __LINE__, fmt, ## __VA_ARGS__)
 #else
 #define LogTrace /* no-op */
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_DEBUG
-#define LogDebug(fmt, ...) LogAtLevel(Debug, fmt, ## __VA_ARGS__)
+#define LogDebug(fmt, ...) LogAtLevel(Debug, __FILE__, __func__, __LINE__, fmt, ## __VA_ARGS__)
 #else
 #define LogDebug /* no-op */
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_INFO
-#define LogInfo(fmt, ...)  LogAtLevel(Info, fmt, ## __VA_ARGS__)
+#define LogInfo(fmt, ...)  LogAtLevel(Info, __FILE__, __func__, __LINE__, fmt, ## __VA_ARGS__)
 #else
 #define LogInfo  /* no-op */
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_WARN
-#define LogWarn(fmt, ...)  LogAtLevel(Warn, fmt, ## __VA_ARGS__)
+#define LogWarn(fmt, ...)  LogAtLevel(Warn, __FILE__, __func__, __LINE__, fmt, ## __VA_ARGS__)
 #else
 #define LogWarn  /* no-op */
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_ERROR
-#define LogError(fmt, ...) LogAtLevel(Error, fmt, ## __VA_ARGS__)
+#define LogError(fmt, ...) LogAtLevel(Error, __FILE__, __func__, __LINE__, fmt, ## __VA_ARGS__)
 #else
 #define LogError /* no-op */
 #endif
 
 #if LOG_LEVEL <= LOG_LEVEL_FATAL
-#define LogFatal(fmt, ...) LogAtLevel(Fatal, fmt, ## __VA_ARGS__)
+#define LogFatal(fmt, ...) LogAtLevel(Fatal, __FILE__, __func__, __LINE__, fmt, ## __VA_ARGS__)
 #else
 #define LogFatal /* no-op */
 #endif
--- src/liblog.c
+++ src/liblog.c
@@ -4,9 +4,16 @@
 
 static enum LogLevel current_log_level = Trace;
 
-int LogAtLevel(enum LogLevel level, const char *fmt, ...) {
+int LogAtLevel(enum LogLevel level,
+               const char *file, const char *func, const int lineNum,
+               const char *fmt, ...) {
 	va_list ap;
 	int rv;
+
+	if( level < current_log_level ) return 0;
+
+	fprintf(stdout, "%s: ", LogLevelName[level]);
+	fprintf(stdout, "from %s:%d (%s): ", file, lineNum, func);
 	va_start(ap,fmt);
 	rv = vfprintf(stdout, fmt, ap);
 	va_end(ap);
