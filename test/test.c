d5517e153d53fa1b5519397c57fc715fd8b6c512
Niels Laukens <niels.laukens@vrt.be>
2013-05-10 10:23:04 +0200

    Added more elaborate testing

--- test/test.c
+++ test/test.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include "../include/liblog_level.h"
+#define LOG_LEVEL LOG_LEVEL_DEBUG
 #include "../include/liblog.h"
 
 int main() {
-	LogTrace("trace log");
+	LogSetLevel(Info);
+	LogTrace("This should compile away");
+	LogDebug("This should not show");
+	LogInfo("This should show");
+
+	LogSetOutput(NULL);
+	LogTrace("This should compile away");
+	LogDebug("This should not show");
+	LogInfo("This should show");
+
 	return 0;
 }
